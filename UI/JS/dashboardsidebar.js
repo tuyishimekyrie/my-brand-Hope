4170fe974e822b194b5786eec37dd49c08c01129
Tuyishime Kyrie <tuyishimekyrie@gmail.com>
2024-02-10 14:56:10 +0200

    implement the responsiveness all pages

--- UI/JS/dashboardsidebar.js
+++ UI/JS/dashboardsidebar.js
@@ -1,6 +1,13 @@
 const sidebar = document.querySelector(".sidebar");
+const nav = document.querySelector(".mobile-nav img");
+const navClose = document.querySelector(".mobile-nav ");
 const menu = document.querySelector(".menu");
 
 menu.addEventListener("click", () => {
   sidebar.classList.toggle("active");
+  navClose.style.display = "flex"
+});
+nav.addEventListener("click", () => {
+  sidebar.classList.remove("active");
+  navClose.style.display="hidden"
 });
